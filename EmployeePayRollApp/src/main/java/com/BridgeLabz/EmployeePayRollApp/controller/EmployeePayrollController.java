16a41ee6e1e2603ac6944e3706d1bea837c3c4a8
Anshi Patel <anshipatel881@gmail.com>
2025-02-27 11:49:11 +0530

    [Anshi] Added Add . Lombok log messages added

--- EmployeePayRollApp/src/main/java/com/BridgeLabz/EmployeePayRollApp/controller/EmployeePayrollController.java
+++ EmployeePayRollApp/src/main/java/com/BridgeLabz/EmployeePayRollApp/controller/EmployeePayrollController.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@Slf4j  // Lombok annotation for logging
 public class EmployeePayrollController {
 
     @Autowired
@@ -16,16 +21,19 @@ public class EmployeePayrollController {
 
     @GetMapping
     public List<EmployeePayrollDTO> getAllEmployees() {
+        log.info("Fetching all employees");
         return employeeService.getAllEmployees();
     }
 
     @PostMapping
     public EmployeePayrollDTO addEmployee(@RequestBody EmployeePayrollDTO employeeDTO) {
+        log.info("Adding new employee: {}", employeeDTO);
         return employeeService.addEmployee(employeeDTO);
     }
 
     @GetMapping("/{id}")
     public EmployeePayrollDTO getEmployeeById(@PathVariable int id) {
+        log.info("Fetching employee with ID: {}", id);
         return employeeService.getEmployeeById(id);
     }
 }
